4ee4b393aeed0f56b3d8d93fafd43808133c2074
yorchtec <a01632080@tec.mx>
2025-09-17 21:38:42 -0600

    Included Gearbox RPM feature
    Into the same transmission ecm. Outputs gearbox rpm from PIN 3 into serial.
    
    Outputs: TIME, Engine_RPM, Gearbox_RPM

--- 1-transmission-ecm/1-transmission-ecm.ino
+++ 1-transmission-ecm/1-transmission-ecm.ino
@@ -1,70 +1,92 @@
+#include <SoftwareSerial.h>
+
 // Ya sirven las interrupciones con freq de sampleo maxima de 90 hz
 // Probado con un simulador embedido: motor_sim
 // Hay que probar con el verdadero motor
 // Crear una rutina de guardado en una memoria sim mientras llega el radiofrequencia
 
 unsigned int ten_ms_sample = 0;
-int rpm_motor_count;
+int rpm_motor_count = 0;
+int rpm_gearbox_count = 0;
 int pulse_per_rev = 8;
 int ten_ms_counter = 0;
-const int sample_time = 200; //200 ms
-float rpm_motor;
+const int sample_time = 200; // 200 ms
+float rpm_motor = 0;
+float rpm_gearbox = 0;
 bool ten_ms_counter_flag = false;
 bool motor_sim = false;
-bool rpm_motor_interrflag;
-
-
+bool rpm_motor_interrflag = false;
+bool rpm_gearbox_interrflag = false;
 
 #define MotorPin 2
+#define GearboxPin 3
 
-
+// Create SoftwareSerial object on pin 8 (TX), pin 7 (RX not used)
+SoftwareSerial mySerial(7, 8); // RX, TX
 
 void setup() {
-
   pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(MotorPin, INPUT);
+  pinMode(GearboxPin, INPUT);
 
-  Serial.begin(9600);
+  Serial.begin(9600);      // USB serial
+  mySerial.begin(9600);    // Software serial on pin 8
 
-  SREG = (SREG & 0b01111111); //Desabilitar interrupciones
-  TIMSK2 = TIMSK2 | 0b00000001; //Habilita la interrupcion por desbordamiento
-  TCCR2B = 0b00000011; //Configura preescala para que FT2 sea de 250 khz
-  SREG = (SREG & 0b01111111) | 0b10000000; //Habilitar interrupciones //Desabilitar interrupciones
+  // Configurar Timer2
+  SREG &= 0b01111111; // Deshabilitar interrupciones
+  TIMSK2 |= 0b00000001; // Habilita la interrupción por desbordamiento
+  TCCR2B = 0b00000011; // Preescala para que FT2 sea de 250 kHz
+  SREG |= 0b10000000; // Habilitar interrupciones
 
-  attachInterrupt(digitalPinToInterrupt(MotorPin), rpm_motor_interrumpt, RISING);
-
+  // Interrupciones externas
+  attachInterrupt(digitalPinToInterrupt(MotorPin), rpm_motor_interrupt, RISING);
+  attachInterrupt(digitalPinToInterrupt(GearboxPin), rpm_gearbox_interrupt, RISING);
 }
-
 
 void loop() {
+  if (ten_ms_counter_flag) {
+    // Print to USB serial
+    Serial.print(millis());
+    Serial.print(",");
+    Serial.print(rpm_motor);
+    Serial.print(",");
+    Serial.println(rpm_gearbox); 
 
-  if (ten_ms_counter_flag == true) {
-    Serial.println(rpm_motor);
-    Serial.println("y");
+    // Print to pin 8 serial
+    mySerial.print(millis());
+    mySerial.print(",");
+    mySerial.print(rpm_motor);
+    mySerial.print(",");
+    mySerial.println(rpm_gearbox); 
+
     ten_ms_counter_flag = false;
-    }
+  }
 }
 
-void rpm_motor_interrumpt() {
+void rpm_motor_interrupt() {
   rpm_motor_interrflag = true;
   rpm_motor_count++;
-
 }
 
-// Timer2 cuenta de 0 a 255, 250E+3/255 = 980.39 -> Por lo tanto cuenta > 9
-ISR(TIMER2_OVF_vect) {
+void rpm_gearbox_interrupt() {
+  rpm_gearbox_interrflag = true;
+  rpm_gearbox_count++;
+}
 
+ISR(TIMER2_OVF_vect) {
   motor_sim = !motor_sim;
   digitalWrite(9, motor_sim);
+  digitalWrite(10, motor_sim);
 
   ten_ms_counter++;
-  if (ten_ms_counter > 195) { // Sample time would be 100 ms
-    rpm_motor = (float (rpm_motor_count)/float(pulse_per_rev)) * (1000/float(sample_time))*60; //Resolution = 13.64 RPM
+  if (ten_ms_counter > 195) {
+    rpm_motor = (float(rpm_motor_count) / pulse_per_rev) * (1000.0 / sample_time) * 60.0;
+    rpm_gearbox = (float(rpm_gearbox_count) / pulse_per_rev) * (1000.0 / sample_time) * 60.0;
+
     rpm_motor_count = 0;
+    rpm_gearbox_count = 0;
     ten_ms_counter_flag = true;
     ten_ms_counter = 0;
   }
-
-
-
-
 }
